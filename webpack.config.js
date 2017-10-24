a9a9708a09ddcde292dff653e41f6da327d56a6b
Alina Ivashchenko <7aliko212@gmail.com>
2017-10-25 02:48:49 +0300

    add pug and pug loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
- 
+
 const PATHS = {
-    source: path.join(__dirname, 'source'),
-    build: path.join(__dirname, 'build')
+	source: path.join(__dirname, 'source'),
+	build: path.join(__dirname, 'build')
 };
- 
+
 module.exports = {
     entry: PATHS.source + '/index.js',
     output: {
@@ -14,7 +14,18 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack app'
+            template: PATHS.source + '/index.pug',
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
+        ]
+    }
 };

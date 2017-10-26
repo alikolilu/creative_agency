75655e532e52dd50a58033678b245aa3d1e12565
Alina Ivashchenko <7aliko212@gmail.com>
2017-10-26 13:04:50 +0300

    separation config on development part and production part

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const PATHS = {
 	build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
     entry: {
         'index': PATHS.source + '/pages/index/index.js',
         'blog': PATHS.source + '/pages/blog/blog.js'
@@ -38,7 +38,25 @@ module.exports = {
             }
         ]
     }
-	devServer: {
-    stats: 'errors-only'
-}
+};
+
+
+const developmentConfig  = {
+    devServer: {
+        stats: 'errors-only',
+		port: 9000
+    }
+};
+
+module.exports = function(env) {
+    if (env === 'production') {
+        return common;
+    }
+    if (env === 'development') {
+        return Object.assign(
+            {},
+            common,
+            developmentConfig
+        );
+    }
 };
